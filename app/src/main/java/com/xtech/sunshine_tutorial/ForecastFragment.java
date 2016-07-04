24a131c669c0dd91b01684c7eeaef80e71e52f96
Mattia Primavera <sconqua@gmail.com>
2016-07-05 00:22:26 +0200

    Adding refresh button + Adding ForecastFragment Menu

--- app/src/main/java/com/xtech/sunshine_tutorial/ForecastFragment.java
+++ app/src/main/java/com/xtech/sunshine_tutorial/ForecastFragment.java
@@ -3,6 +3,9 @@ package com.xtech.sunshine_tutorial;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -18,7 +21,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-
+        setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         String[] forecastArray = {
@@ -46,6 +49,30 @@ public class ForecastFragment extends Fragment {
         listView.setAdapter(adapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask task = new FetchWeatherTask();
+            task.execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
