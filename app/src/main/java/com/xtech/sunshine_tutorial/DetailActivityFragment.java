f709a096f2bf801855eed8ef04f3694856e5bbcf
Mattia Primavera <sconqua@gmail.com>
2016-07-07 22:34:46 +0200

    Showing detail data

--- app/src/main/java/com/xtech/sunshine_tutorial/DetailActivityFragment.java
+++ app/src/main/java/com/xtech/sunshine_tutorial/DetailActivityFragment.java
@@ -1,10 +1,13 @@
 package com.xtech.sunshine_tutorial;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,12 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecast);
+        }
+        return rootView;
     }
 }
