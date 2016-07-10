58f5ea53d24aeb85327f95cf2e6d6c3234a4caa8
Mattia Primavera <sconqua@gmail.com>
2016-07-10 14:14:33 +0200

    Settings Activity Listener + Layout
    - Summary set on option update
    - Settings strings
    - onPause and onResume methods

--- app/src/main/java/com/xtech/sunshine_tutorial/SettingsActivity.java
+++ app/src/main/java/com/xtech/sunshine_tutorial/SettingsActivity.java
@@ -1,21 +1,27 @@
 package com.xtech.sunshine_tutorial;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
-public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        Log.d("Preferences: ", "Setting summary for preference *************");
 
+        Preference connectionPref = findPreference(key);
+        // Set summary to be the user-description for the selected value
+        connectionPref.setSummary(sharedPreferences.getString(key, ""));
+    }
 
     @Override
     public void onStart() {
@@ -25,5 +31,19 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
     @Override
     public void onStop() {
         super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
