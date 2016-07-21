6bfec2b837602decdebe02e404a3a9e228c2e412
Mattia Primavera <sconqua@gmail.com>
2016-07-21 19:57:45 +0200

    TestUtils updated

--- app/src/androidTest/java/com/xtech/sunshine_tutorial/TestUtils.java
+++ app/src/androidTest/java/com/xtech/sunshine_tutorial/TestUtils.java
@@ -1,16 +1,40 @@
 package com.xtech.sunshine_tutorial;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
+
 import java.util.Map;
 import java.util.Set;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class TestUtils {
-    public static ContentValues createInsertValues(){
+    public static final String TEST_DATE = "22-01-2016";
+    public static final String TEST_LOC = "North Pole";
+
+    public static ContentValues createInsertLocationValues(){
         ContentValues result = new ContentValues();
-        result.put(WeatherContract.LocationEntry.COL_CITY_NAME, "Paris");
+        result.put(WeatherContract.LocationEntry.COL_CITY_NAME, "North Pole");
+        return result;
+    }
+
+    public static ContentValues createInsertWeatherValues(long locationId){
+        ContentValues result = new ContentValues();
+        result.put(WeatherContract.WeatherEntry.COL_LOC_KEY, locationId);
+        result.put(WeatherContract.WeatherEntry.COL_DATE, "22-01-2016");
+        result.put(WeatherContract.WeatherEntry.COL_DESC, "sunny");
+        result.put(WeatherContract.WeatherEntry.COL_HUMIDITY, "2");
+        result.put(WeatherContract.WeatherEntry.COL_MAX_TEMP, "22");
+        result.put(WeatherContract.WeatherEntry.COL_MIN_TEMP, "15");
+        result.put(WeatherContract.WeatherEntry.COL_WEATHER_ID, "54651");
         return result;
     }
 
@@ -25,5 +49,110 @@ public class TestUtils {
                     "' did not match the expected value '" +
                     expectedValue + "'. " + error, expectedValue, valueCursor.getString(idx));
         }
+    }
+
+    static ContentValues createNorthPoleLocationValues() {
+        // Create a new map of values, where column names are the keys
+        ContentValues testValues = new ContentValues();
+        testValues.put(WeatherContract.LocationEntry.COL_CITY_NAME, "North Pole");
+        return testValues;
+    }
+
+    /*
+        The functions we provide inside of TestProvider use this utility class to test
+        the ContentObserver callbacks using the PollingCheck class that we grabbed from the Android
+        CTS tests.
+
+        Note that this only tests that the onChange function is called; it does not test that the
+        correct Uri is returned.
+     */
+    static class TestContentObserver extends ContentObserver {
+        final HandlerThread mHT;
+        boolean mContentChanged;
+
+        static TestContentObserver getTestContentObserver() {
+            HandlerThread ht = new HandlerThread("ContentObserverThread");
+            ht.start();
+            return new TestContentObserver(ht);
+        }
+
+        private TestContentObserver(HandlerThread ht) {
+            super(new Handler(ht.getLooper()));
+            mHT = ht;
+        }
+
+        // On earlier versions of Android, this onChange method is called
+        @Override
+        public void onChange(boolean selfChange) {
+            onChange(selfChange, null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            mContentChanged = true;
+        }
+
+        public void waitForNotificationOrFail() {
+            // Note: The PollingCheck class is taken from the Android CTS (Compatibility Test Suite).
+            // It's useful to look at the Android CTS source for ideas on how to test your Android
+            // applications.  The reason that PollingCheck works is that, by default, the JUnit
+            // testing framework is not running on the main Android application thread.
+            new PollingCheck(5000) {
+                @Override
+                protected boolean check() {
+                    return mContentChanged;
+                }
+            }.run();
+            mHT.quit();
+        }
+    }
+
+    static TestContentObserver getTestContentObserver() {
+        return TestContentObserver.getTestContentObserver();
+    }
+
+    static void validateCurrentRecord(String error, Cursor valueCursor, ContentValues expectedValues) {
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse("Column '" + columnName + "' not found. " + error, idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals("Value '" + entry.getValue().toString() +
+                    "' did not match the expected value '" +
+                    expectedValue + "'. " + error, expectedValue, valueCursor.getString(idx));
+        }
+    }
+
+    static void validateCursor(String error, Cursor valueCursor, ContentValues expectedValues) {
+        assertTrue("Empty cursor returned. " + error, valueCursor.moveToFirst());
+        validateCurrentRecord(error, valueCursor, expectedValues);
+        valueCursor.close();
+    }
+
+    static ContentValues createWeatherValues(long locationRowId) {
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherContract.WeatherEntry.COL_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherContract.WeatherEntry.COL_DATE, TEST_DATE);
+        weatherValues.put(WeatherContract.WeatherEntry.COL_MAX_TEMP, 75);
+        weatherValues.put(WeatherContract.WeatherEntry.COL_MIN_TEMP, 65);
+        weatherValues.put(WeatherContract.WeatherEntry.COL_DESC, "Asteroids");
+        weatherValues.put(WeatherContract.WeatherEntry.COL_WEATHER_ID, 321);
+        weatherValues.put(WeatherContract.WeatherEntry.COL_HUMIDITY, 20);
+        return weatherValues;
+    }
+    static long insertNorthPoleLocationValues(Context context) {
+        // insert our test records into the database
+        WeatherDbHelper dbHelper = new WeatherDbHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues testValues = TestUtils.createNorthPoleLocationValues();
+
+        long locationRowId;
+        locationRowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, testValues);
+
+        // Verify we got a row back.
+        assertTrue("Error: Failure to insert North Pole Location Values", locationRowId != -1);
+
+        return locationRowId;
     }
 }
